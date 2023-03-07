88b236c784bd5f06b14286c9351e61bc0259b77d
vladislava-ar <vladislava.dnk@gmail.com>
2023-03-07 13:38:43 +0200

    rename variables and function

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let a = document.getElementById('candlePrice').innerHTML;
-let b = document.getElementById('mascaraPrice').innerHTML;
-let c = document.getElementById('saltPrice').innerHTML;
-let d = document.getElementById('lipstickPrice').innerHTML;
-let count = 0.8*(+a + +b + +c + +d); 
+const a = document.getElementById('candlePrice').innerHTML;
+const b = document.getElementById('mascaraPrice').innerHTML;
+const c = document.getElementById('saltPrice').innerHTML;
+const d = document.getElementById('lipstickPrice').innerHTML;
+const count = 0.8*(+a + +b + +c + +d); 
 
-function result() {
+function getResult() {
     let total = document.getElementById('total');
 	total.textContent = count + ' руб.';
 }
